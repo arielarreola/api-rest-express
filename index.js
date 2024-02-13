0db518d3707e51aae18572a33d410fd983011097
unknown <arielyohaina.arreolablanco@umg.edu.mx>
2024-02-13 10:44:31 -0600

    13/02/24 Modificacion nuevo endpoint

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000;
 app.get("/", (req, res) => {
     res.send('Ariel Arreola');
 })
+app.get("/hola", (req, res) => {
+    res.send('Bienvenidos a todos');
+})
 
 app.listen(port, ()=>{
     console.log('Escuchando el puerto',port);
